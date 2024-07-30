013610a835533cacc51c0170dd30991a5982285e
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2024-07-30 04:01:26 +0100

    Fixed issue where menu would not display when icon was right clicked

--- MineLights.cpp
+++ MineLights.cpp
@@ -1,7 +1,10 @@
-// Include necessary headers
 #include <Windows.h>
 #include "Resource.h"
 #include "iCueLightController.h"
+
+// Define constants
+#define IDM_EXIT 1001
+#define TRAY_ICON_MESSAGE (WM_USER + 1)
 
 // Declare global variables
 NOTIFYICONDATA nid;
@@ -12,7 +15,7 @@ void InitTrayIcon(HWND hWnd) {
     nid.hWnd = hWnd;
     nid.uID = 1;
     nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
-    nid.uCallbackMessage = WM_USER + 1;
+    nid.uCallbackMessage = TRAY_ICON_MESSAGE;
     //nid.hIcon = LoadIcon(GetModuleHandle(nullptr), MAKEINTRESOURCE(IDI_ICON1)); // Use your custom icon
     lstrcpy(nid.szTip, L"MineLights"); // Tooltip text
     Shell_NotifyIcon(NIM_ADD, &nid);
@@ -21,21 +24,24 @@ void InitTrayIcon(HWND hWnd) {
 // Handle tray icon messages
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
-    case WM_USER + 1: // Tray icon clicked
+    case TRAY_ICON_MESSAGE: // Tray icon clicked
         if (lParam == WM_RBUTTONDOWN) {
             // Show context menu
             HMENU hmenu = CreatePopupMenu();
             InsertMenu(hmenu, 0, MF_BYPOSITION | MF_STRING, IDM_EXIT, L"Exit");
             POINT pt;
             GetCursorPos(&pt);
+            SetForegroundWindow(hWnd); // Necessary to ensure the menu is shown
             TrackPopupMenu(hmenu, TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_BOTTOMALIGN, pt.x, pt.y, 0, hWnd, NULL);
+            DestroyMenu(hmenu);
         }
         break;
 
     case WM_COMMAND:
         if (LOWORD(wParam) == IDM_EXIT) {
             // Clean up and exit the application
-            DestroyWindow(hWnd);
+            Shell_NotifyIcon(NIM_DELETE, &nid);
+            PostQuitMessage(0);
         }
         break;
     }
@@ -48,24 +54,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     WNDCLASS wc = { 0 };
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
-    wc.lpszClassName = L"MyAppClass";
+    wc.lpszClassName = L"MineLightsClass";
     RegisterClass(&wc);
-    HWND hWnd = CreateWindow(wc.lpszClassName, L"MineLights", 0, 0, 0, 0, 0, HWND_MESSAGE, nullptr, hInstance, nullptr);
 
-    // Initialize the tray icon
+    HWND hWnd = CreateWindow(L"MineLightsClass", L"MineLights", 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, hInstance, NULL);
+    if (!hWnd) {
+        return 0;
+    }
+
     InitTrayIcon(hWnd);
-
-    iCueLightController();
-
 
     // Message loop
     MSG msg;
-    while (GetMessage(&msg, nullptr, 0, 0)) {
+    while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
-    // Clean up
-    Shell_NotifyIcon(NIM_DELETE, &nid);
-    return 0;
+    return (int)msg.wParam;
 }
