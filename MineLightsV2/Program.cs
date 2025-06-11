79c8f947202c6859f9cedb485de37e76ae667675
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-06-11 02:45:10 +0100

    Updated to be a Windows Tray application, added icon

--- MineLightsV2/Program.cs
+++ MineLightsV2/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread trayThread = new Thread(new ThreadStart(RunTrayApp));
+        trayThread.SetApartmentState(ApartmentState.STA);
+        trayThread.Start();
+        trayThread.Join();
+    }
+
+    private static void RunTrayApp()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MyAppContext());
+    }
+}
+
+public class MyAppContext : ApplicationContext
+{
+    private NotifyIcon trayIcon;
+
+    public MyAppContext()
+    {
+        trayIcon = new NotifyIcon()
+        {
+            Icon = new Icon(Assembly.GetExecutingAssembly().GetManifestResourceStream("MineLightsV2.resources.app_icon.ico")),
+            Text = "MineLights Server",
+            ContextMenuStrip = new ContextMenuStrip(),
+            Visible = true
+        };
+
+        trayIcon.ContextMenuStrip.Items.Add("Show Message", null, OnShowVersion);
+        trayIcon.ContextMenuStrip.Items.Add("Exit", null, OnExit);
+    }
+
+    void OnShowVersion(object sender, EventArgs e)
+    {
+        MessageBox.Show("MineLights Server V2");
+    }
+
+    void OnExit(object sender, EventArgs e)
+    {
+        trayIcon.Visible = false;
+        Application.Exit();
+    }
+}
